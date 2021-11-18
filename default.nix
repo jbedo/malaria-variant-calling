ce8fa06c2cf01a36785d84f31597161cf401415e
Justin Bedo <cu@cua0.org>
2021-11-19 07:18:24 +1100

    use tree fold instead of left fold

--- default.nix
+++ default.nix
@@ -12,6 +12,13 @@ let
   PMIX = "PvP01_13_v2:876959-881753";
   PMX = "PvP01_01_v2:555499-559411";
 
+  foldt1 = f: xs: if length xs > 1 then foldt1 f (foldp1 f xs) else head xs;
+  foldp1 = f: xs:
+    if length xs > 1 then
+      [ (f (head xs) (head (tail xs))) ] ++ foldp1 f (tail (tail xs))
+    else
+      xs;
+
   ena = pkgs.callPackage ./ena.nix { inherit fetchFastQGZ; };
   foldl1 = f: xs: foldl f (head xs) (tail xs);
   samples = ena.importFromJSON ./PRJEB2140.json;
@@ -20,7 +27,7 @@ let
     (mapAttrsToList (id: bwa.align { inherit ref; flags = "-R'@RG\\tID:${id}\\tSM:${id}'"; }))
     (map (samtools.sort { }))
     (map (gatk.callHaplotype { targets = [ PMIX PMX ]; }))
-    (foldl1 (a: b: gatk.merge { } { inherit a b; }))
+    (foldt1 (a: b: gatk.merge { } { inherit a b; }))
     (gatk.callGenotypes { })
   ];
 
